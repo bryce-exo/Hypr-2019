52415fa789e5a7a6e8752ae4eee632293ac68e61
Bryce <bryce@exostudio.com>
2019-03-21 17:14:51 -0500

    Initial commit

---
+++ wp-content/themes/hypr/assets/js/scripts.js
@@ -0,0 +1,5 @@
+jQuery(function($){
+   
+	$('body').addClass('pageload');
+
+});
---
+++ wp-content/themes/hypr/functions.php
@@ -0,0 +1,44 @@
+<?php
+
+/**
+
+ * @author Divi Space
+
+ * @copyright 2017
+
+ */
+
+if (!defined('ABSPATH')) die();
+
+
+
+function ds_ct_enqueue_parent() { wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' ); }
+
+
+
+function ds_ct_loadjs() {
+
+	wp_enqueue_script( 'ds-theme-script', get_stylesheet_directory_uri() . '/ds-script.js',
+
+        array( 'jquery' )
+
+    );
+
+}
+
+function mycustomscript_enqueue() {
+    wp_enqueue_script( 'custom-scripts', get_stylesheet_directory_uri() . '/assets/js/scripts.js', array ( 'jquery') );
+}
+add_action( 'wp_enqueue_scripts', 'mycustomscript_enqueue' );
+
+
+
+add_action( 'wp_enqueue_scripts', 'ds_ct_enqueue_parent' );
+
+add_action( 'wp_enqueue_scripts', 'ds_ct_loadjs' );
+
+
+
+include('login-editor.php');
+
+?>
