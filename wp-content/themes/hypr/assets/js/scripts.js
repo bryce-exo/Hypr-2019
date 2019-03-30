8f100085b14afdafe908079cd5b84358a595bdaa
Bryce Thompson <bryce@exostudio.com>
2019-03-29 19:09:08 -0500

    Updated site global styling

--- wp-content/themes/hypr/assets/js/scripts.js
+++ wp-content/themes/hypr/assets/js/scripts.js
@@ -2,4 +2,14 @@ jQuery(function($){
    
 	$('body').addClass('pageload');
 
+
+
+	const el = document.querySelector(".masthead-section");
+
+	el.addEventListener("mousemove", (e) => {
+		
+	  el.style.backgroundPositionX = -e.offsetX + "px";
+	  el.style.backgroundPositionY = -e.offsetY + "px";
+	});
+
 });
