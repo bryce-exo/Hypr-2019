971672b4586913a3188584d8baeb4e52990418af
Bryce Thompson <bryce@exostudio.com>
2019-04-02 23:58:55 -0500

    demo

--- wp-content/themes/hypr/layout/getdemo.php
+++ wp-content/themes/hypr/layout/getdemo.php
@@ -7,23 +7,32 @@
 
 get_header(); ?>
 
-<div class="container">
-	<div class="row">
+
+
+
+<div class="masthead">
+
+	<div class="row logo-container">
 		<a class="demo-logo" href="/">Hypr</a>
 	</div>
-</div>
-
-
-<div class="container">
-	<div class="row">
+	<div class="row switch">
 		<div class="col">
 			<ul id="switch-content">
 				<li id="one" class="switch-container">
-					<h1 class="title">Schedule</h1>
-					<h1>A Demo</h1>
-					<?php echo do_shortcode('[showmodule id="1907"]'); ?>
-
-
+					<div class="col-1">
+						<div class="wrap">
+						<h1>Schedule</h1>
+						<h4>A Demo</h4>
+						<p>	
+							Schedule your 1-on-1 Demo with our team of experts and learn why the world’s largest companies choose HYPR to eliminate passwords.</p>
+						</div>
+					</div>
+					<div class="col-2">
+						<div class="wrap">
+						<?php echo do_shortcode('[showmodule id="1907"]'); ?>
+						<small>HYPR may retain your contact information for future communications. For more information on our privacy practices, please review our Privacy Policy.</small>
+						</div>
+					</div>
 				</li>
 
 				<li id="experience" class="switch-container active enter">
@@ -32,29 +41,54 @@ get_header(); ?>
 					</a>
 				</li>
 
-				
+
+
+				<div class="subnav">
+					<div class="wrap">
+					<div class="row">
+						<div class="col">
+							<button id="one-btn" class="btn-demo">1 on 1</button>
+						</div>
+						<div class="col active">
+							<button id="experience-btn" class="btn-demo">Experience</button>
+						</div>
+						<div class="col last">
+							<a target="_blank" href="https://google.com" id="tour-btn" class="btn-demo">Tour</a>
+						</div>
+					</div>
+					</div>
+				</div>
 			</ul>
+
+			<div class="badges">
+				<div class="wrap">
+					<a class="apple-store" href="" style="background-image: url('https://dctfcu.com/wp-content/uploads/sites/63/2018/04/app-store-logo.png');"></a>
+					<a class="play-store" href="" style="background-image: url('https://static-content.4tellus.com/wp-content/uploads/2018/05/23014710/badge-play.png');"></a>
+				</div>
+			</div>
 		</div>
 	</div>
 
 	
+
+	
 </div>
 
-<div class="et_pb_section">
-	<div class="et_pb_row et_pb_row_6">
-		<div class="et_pb_column et_pb_column_1_3">
-			<button id="one-btn" class="btn-demo">1 on 1</button>
-		</div>
-		<div class="et_pb_column et_pb_column_1_3 active">
-			<button id="experience-btn" class="btn-demo">Experience</button>
-		</div>
-		<div class="et_pb_column et_pb_column_1_3 last">
-			<a target="_blank" href="https://google.com" id="tour-btn" class="btn-demo">Tour</button>
+<div class="container videos">
+	<div class="row text-module">
+		<div class="wrap">
+		<h5>HOW WE'RE DIFFERENT</h5>
+		<h1>Watch</h1>
+		<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
 		</div>
 	</div>
+
+	<?php echo do_shortcode('[showmodule id="1911"]'); ?>
 </div>
 
 
+
+<?php echo do_shortcode('[showmodule id="392"]'); ?>
 
 
 
